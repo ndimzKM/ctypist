60ff5393986b7f8c0f428dfb6be4a8e48ab57e61
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-04-18 20:04:01 +0000

    new code snippets

--- src/faker/index.ts
+++ src/faker/index.ts
@@ -3,4 +3,8 @@ const code1: string =
 export const code: string =
   "const getUserNotes = () => (resolve, reject) => {\n\tconst notes: any = localStorage.getItem('notes')\n\tif (!notes) {\n\t\tresolve([scratchpadNote, welcomeNote])\n\t} else if (Array.isArray(parseArray(notes))) {\n\t\tresolve(\n\t\t\tnotes.length === 0 || !notes.find((note) => note.id)\n\t\t\t? [scratchNote, ...notes]\n\t\t\t: JSON.parse(notes)\n\t\t)\n\t} else {\n\t\treject({\n\t\t\tmessage: 'Something went wrong',\n\t\t})\n\t}\n}";
 
-export const snippets: string[] = [code1, code];
+const code2: string =
+  "function parseMiddleware(args){\n\tlet middleware = {callbacks:[]}\n\tfor(let arg of args){\n\t\tif(typeof(arg) == 'string'){\n\t\t\tmiddleware.path = arg\n\t\t}else{\n\t\t\tmiddleware.callbacks.push(arg);\n\t\t}\n\t}\n\treturn middleware\n}\nmodule.exports = { parseMiddleware }";
+const code3: string =
+  'function append(name, value) {\n\tif (typeof value != "string")\n\t\tthrow new Error("value argument must be of type string");\n\tif (res.headers[name.toLowerCase()] == undefined) {\n\t\tres.setHeader(name, value);\n\t} else {\n\t\tres.headers[name.toLowerCase()] += value;\n\t}\n};';
+export const snippets: string[] = [code1, code, code2, code3];
