1aae3c74bc659e50b8f0c38d7b15c6d8b845edb9
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-04-21 16:33:59 +0000

    new code snippet

--- src/faker/index.ts
+++ src/faker/index.ts
@@ -7,4 +7,6 @@ const code2: string =
   "function parseMiddleware(args){\n\tlet middleware = {callbacks:[]}\n\tfor(let arg of args){\n\t\tif(typeof(arg) == 'string'){\n\t\t\tmiddleware.path = arg\n\t\t}else{\n\t\t\tmiddleware.callbacks.push(arg);\n\t\t}\n\t}\n\treturn middleware\n}\nmodule.exports = { parseMiddleware }";
 const code3: string =
   'function append(name, value) {\n\tif (typeof value != "string")\n\t\tthrow new Error("value argument must be of type string");\n\tif (res.headers[name.toLowerCase()] == undefined) {\n\t\tres.setHeader(name, value);\n\t} else {\n\t\tres.headers[name.toLowerCase()] += value;\n\t}\n};';
-export const snippets: string[] = [code1, code, code2, code3];
+const code4: string =
+  'socket.on("WebhookEvent", (data) => {\n    socket.broadcast.emit("new message", {\n      username: socket.username,\n      message: data,\n    });\n  });\n  socket.on("disconnect", () => {\n    users -= 1;\n    console.log("Users: ", users);\n  });';
+export const snippets: string[] = [code1, code, code2, code3, code4];
